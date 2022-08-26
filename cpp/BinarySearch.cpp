08a0094a3beff4e08e52162b8df3d61751a86742
Adwit Mittal <adwitmittal@gmail.com>
2022-08-26 16:27:56 +0530

    added stack

--- cpp/BinarySearch.cpp
+++ cpp/BinarySearch.cpp
@@ -1,26 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int binarySearch(int arr[], int l, int r, int x)
+int bSearch(int arr[], int l, int r, int item)
 {
-    if (r >= l) {
-        int mid = l + (r - l) / 2;
-        if (arr[mid] == x)
-            return mid;
-            return binarySearch(arr, l, mid - 1, x);
-        return binarySearch(arr, mid + 1, r, x);
-    }
-    return -1;
+	if (r >= l) {
+		int mid = l + (r - l) / 2;
+		if (arr[mid] == item)
+			return mid;
+
+		if (arr[mid] > item)
+			return bSearch(arr, l, mid - 1, item);
+
+		return bSearch(arr, mid + 1, r, item);
+	}
+	return -1;
 }
- 
+
 int main(void)
 {
-    int arr[] = { 2, 3, 4, 10, 40 };
-    int x = 10;
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int result = binarySearch(arr, 0, n - 1, x);
-    (result == -1)
-        ? cout << "Element is not present in array"
-        : cout << "Element is present at index " << result;
-    return 0;
+	int arr[] = { 2, 3, 4, 10, 40 };
+	int item = 10;
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int result = bSearch(arr, 0, n - 1, item);
+	if(result == -1)
+		cout<<"Element not present";
+	else
+		cout<< "Element is present at position " << result<<endl;
 }
